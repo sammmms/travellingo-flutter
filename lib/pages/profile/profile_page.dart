0f33f5de35e26f416bd4c6636dd5247ada852820
Sam! <156488637+sammmms@users.noreply.github.com>
2024-06-07 22:34:52 +0700

    feat: added stream for easier editing

--- lib/pages/profile/profile_page.dart
+++ lib/pages/profile/profile_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter_localization/flutter_localization.dart';
 import 'package:provider/provider.dart';
 import 'package:travellingo/bloc/user_bloc/user_state.dart';
 import 'package:travellingo/component/my_title.dart';
-import 'package:travellingo/component/snackbar_component.dart';
 import 'package:travellingo/component/transition_animation.dart';
 import 'package:travellingo/models/user.dart';
 import 'package:travellingo/bloc/user_bloc/user_bloc.dart';
@@ -35,135 +34,151 @@ class _ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "profile".getString(context),
-            style: const TextStyle(fontWeight: FontWeight.bold),
+    return MultiProvider(
+      providers: [
+        Provider<UserBloc>.value(value: bloc),
+      ],
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              "profile".getString(context),
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            scrolledUnderElevation: 0,
           ),
-          scrolledUnderElevation: 0,
-        ),
-        body: StreamBuilder<UserState>(
-            stream: bloc.controller.stream,
-            builder: (context, snapshot) {
-              if (!snapshot.hasData || (snapshot.data?.loading ?? false)) {
-                return SizedBox(
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height,
-                    child: const Center(child: CircularProgressIndicator()));
-              }
-              if (snapshot.data!.error) {
-                WidgetsBinding.instance.addPostFrameCallback((_) {
-                  showMySnackBar(
-                      context, snapshot.data?.errorMessage ?? "somethingWrong");
-                });
-              }
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                StreamBuilder<UserState>(
+                    stream: bloc.controller,
+                    builder: (context, snapshot) {
+                      if (!snapshot.hasData ||
+                          (snapshot.data?.loading ?? false)) {
+                        return const Expanded(
+                            child: Center(child: CircularProgressIndicator()));
+                      }
+                      if (snapshot.data!.error) {
+                        return Expanded(
+                          child: Center(
+                              child: Text("somethingWrong".getString(context))),
+                        );
+                      }
 
-              if (snapshot.data?.receivedProfile == null) {
-                return SizedBox(
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height,
-                    child: const Center(child: CircularProgressIndicator()));
-              }
-              User data = snapshot.data!.receivedProfile!;
-              WidgetsBinding.instance.addPostFrameCallback((_) {
-                Provider.of<UserDetailProvider>(context, listen: false)
-                    .updateUser(data);
-              });
-              return SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Center(
+                      if (snapshot.data?.receivedProfile == null) {
+                        return Expanded(
+                          child: Center(
+                              child: Text("somethingWrong".getString(context))),
+                        );
+                      }
+                      User data = snapshot.data!.receivedProfile!;
+                      // print(data.pictureLink);
+                      return RefreshIndicator(
+                        onRefresh: () async {
+                          bloc.getUser();
+                        },
+                        child: SingleChildScrollView(
+                          physics: const AlwaysScrollableScrollPhysics(),
                           child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              BorderedAvatar(content: data.pictureLink),
-                              const SizedBox(
-                                height: 5,
-                              ),
-                              Text(
-                                data.name,
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.bold),
-                                textScaler: const TextScaler.linear(1.1),
-                              ),
-                              const SizedBox(
-                                height: 5,
-                              ),
-                              Text(data.email)
-                            ],
-                          ),
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Center(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      BorderedAvatar(content: data.pictureLink),
+                                      const SizedBox(
+                                        height: 5,
+                                      ),
+                                      Text(
+                                        data.name,
+                                        style: const TextStyle(
+                                            fontWeight: FontWeight.bold),
+                                        textScaler:
+                                            const TextScaler.linear(1.1),
+                                      ),
+                                      const SizedBox(
+                                        height: 5,
+                                      ),
+                                      Text(data.email)
+                                    ],
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                                const MyTitle(title: "account"),
+                                TextNavigator(
+                                  needIcon: true,
+                                  onTapFunction: () {
+                                    Navigator.of(context).push(slideInFromRight(
+                                        Provider<UserBloc>.value(
+                                            value: bloc,
+                                            child: const PersonalInfoPage())));
+                                  },
+                                  text: "personalInfo",
+                                ),
+                                TextNavigator(
+                                  needIcon: true,
+                                  onTapFunction: () {
+                                    Navigator.of(context).push(slideInFromRight(
+                                        const PrivacySharingPage()));
+                                  },
+                                  text: "privacyNSharing",
+                                ),
+                                const Divider(
+                                  height: 1,
+                                  color: Color(0xFFF6F8FB),
+                                  indent: 20,
+                                  endIndent: 20,
+                                ),
+                                const MyTitle(title: "settings"),
+                                TextNavigator(
+                                  onTapFunction: () {
+                                    Navigator.push(
+                                        context,
+                                        slideInFromRight(
+                                            const NotificationPage()));
+                                  },
+                                  text: "notification",
+                                  needIcon: true,
+                                ),
+                                TextNavigator(
+                                  onTapFunction: () {
+                                    Navigator.of(context).push(slideInFromRight(
+                                        const AppearancePage()));
+                                  },
+                                  text: "appearance",
+                                  needIcon: true,
+                                ),
+                                TextNavigator(
+                                  onTapFunction: () {},
+                                  text: "purchaseHistory",
+                                ),
+                                TextNavigator(
+                                  onTapFunction: () {},
+                                  text: "review",
+                                ),
+                                TextNavigator(
+                                  onTapFunction: () {
+                                    context.read<UserDetailProvider>().user =
+                                        null;
+                                    ResetPreferences.removeToken();
+                                    Navigator.of(context).pushAndRemoveUntil(
+                                        slideInFromBottom(const SignInPage()),
+                                        (route) => false);
+                                  },
+                                  text: "logout",
+                                  style: const TextStyle(color: Colors.red),
+                                ),
+                              ]),
                         ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        const MyTitle(title: "account"),
-                        TextNavigator(
-                          needIcon: true,
-                          onTapFunction: () {
-                            Navigator.of(context).push(slideInFromRight(
-                                Provider<UserBloc>.value(
-                                    value: bloc,
-                                    child: const PersonalInfoPage())));
-                          },
-                          text: "personalInfo",
-                        ),
-                        TextNavigator(
-                          needIcon: true,
-                          onTapFunction: () {
-                            Navigator.of(context).push(
-                                slideInFromRight(const PrivacySharingPage()));
-                          },
-                          text: "privacyNSharing",
-                        ),
-                        const Divider(
-                          height: 1,
-                          color: Color(0xFFF6F8FB),
-                          indent: 20,
-                          endIndent: 20,
-                        ),
-                        const MyTitle(title: "settings"),
-                        TextNavigator(
-                          onTapFunction: () {
-                            Navigator.push(context,
-                                slideInFromRight(const NotificationPage()));
-                          },
-                          text: "notification",
-                          needIcon: true,
-                        ),
-                        TextNavigator(
-                          onTapFunction: () {
-                            Navigator.of(context)
-                                .push(slideInFromRight(const AppearancePage()));
-                          },
-                          text: "appearance",
-                          needIcon: true,
-                        ),
-                        TextNavigator(
-                          onTapFunction: () {},
-                          text: "purchaseHistory",
-                        ),
-                        TextNavigator(
-                          onTapFunction: () {},
-                          text: "review",
-                        ),
-                        TextNavigator(
-                          onTapFunction: () {
-                            context.read<UserDetailProvider>().user = null;
-                            ResetPreferences.removeToken();
-                            Navigator.of(context).pushAndRemoveUntil(
-                                slideInFromBottom(const SignInPage()),
-                                (route) => false);
-                          },
-                          text: "logout",
-                          style: const TextStyle(color: Colors.red),
-                        ),
-                      ]),
-                ),
-              );
-            }));
+                      );
+                    }),
+              ],
+            ),
+          )),
+    );
   }
 }
