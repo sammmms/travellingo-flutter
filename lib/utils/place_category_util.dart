fc7cd6f2146ccd66744a0408421d4deabbc1c407
sammmms <samuelonasis23@gmail.com>
2024-06-25 15:18:25 +0700

    feat: add place category util

---
+++ lib/utils/place_category_util.dart
@@ -0,0 +1,21 @@
+enum PlaceCategory { hotel, restaurant, attraction, zoo, all }
+
+class PlaceCategoryUtil {
+  static const Map<PlaceCategory, String> _placeCategoryMap = {
+    PlaceCategory.hotel: 'Hotel',
+    PlaceCategory.restaurant: 'Restaurant',
+    PlaceCategory.attraction: 'Attraction',
+    PlaceCategory.zoo: 'Zoo',
+    PlaceCategory.all: 'All'
+  };
+
+  static String stringOf(PlaceCategory placeCategory) {
+    return _placeCategoryMap[placeCategory]!.toLowerCase();
+  }
+
+  static PlaceCategory categoryOf(String placeCategory) {
+    return _placeCategoryMap.entries
+        .firstWhere((element) => element.value.toLowerCase() == placeCategory)
+        .key;
+  }
+}
