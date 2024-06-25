301ec8d2306f75af2223b771867885ef799912f4
sammmms <samuelonasis23@gmail.com>
2024-06-25 15:12:00 +0700

    feat: add review model

---
+++ lib/models/review.dart
@@ -0,0 +1,43 @@
+class Review {
+  String id;
+  String user;
+  String place;
+  double rating;
+  String review;
+  bool haveUpdated;
+  DateTime createdAt;
+
+  Review({
+    required this.id,
+    required this.user,
+    required this.place,
+    required this.rating,
+    required this.review,
+    required this.haveUpdated,
+    required this.createdAt,
+  });
+
+  factory Review.fromJson(Map<String, dynamic> json) {
+    return Review(
+      id: json["_id"],
+      user: json["user"],
+      place: json["place"],
+      rating: double.tryParse(json["rating"].toString()) ?? 0,
+      review: json["review"],
+      haveUpdated: json["haveUpdated"],
+      createdAt: DateTime.parse(json["createdAt"]),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "_id": id,
+      "user": user,
+      "place": place,
+      "rating": rating,
+      "review": review,
+      "haveUpdated": haveUpdated,
+      "createdAt": createdAt,
+    };
+  }
+}
