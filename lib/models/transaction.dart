ff3de71ecc9170122628d0fc024d9aa0f7bb1123
sammmms <samuelonasis23@gmail.com>
2024-06-27 17:00:52 +0700

    feat: added transaction models

---
+++ lib/models/transaction.dart
@@ -0,0 +1,74 @@
+import 'package:travellingo/models/place.dart';
+import 'package:travellingo/utils/transaction_status_util.dart';
+
+class TransactionItems {
+  final String id;
+  final Place place;
+  final int quantity;
+
+  TransactionItems({
+    required this.id,
+    required this.place,
+    required this.quantity,
+  });
+
+  factory TransactionItems.fromJson(Map<String, dynamic> json) {
+    return TransactionItems(
+      id: json['_id'],
+      place: Place.fromJson(json['place']),
+      quantity: json['quantity'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'place': place.toJson(), 'quantity': quantity};
+  }
+}
+
+class Transaction {
+  final String id;
+  final List<TransactionItems> items;
+  final int total;
+  final DateTime transactionDate;
+  final DateTime expiredAt;
+  final TransactionStatus status;
+
+  Transaction({
+    required this.id,
+    this.items = const [],
+    required this.total,
+    required this.transactionDate,
+    required this.expiredAt,
+    required this.status,
+  });
+
+  factory Transaction.fromJson(Map<String, dynamic> json) {
+    List? jsonItems = json['items'];
+    late List<TransactionItems> items;
+    if (jsonItems != null) {
+      items = jsonItems.map((e) => TransactionItems.fromJson(e)).toList();
+    } else {
+      items = [];
+    }
+
+    return Transaction(
+      items: items,
+      id: json['_id'],
+      total: json['total'],
+      transactionDate: DateTime.parse(json['transactionDate']),
+      expiredAt: DateTime.parse(json['expiredAt']),
+      status: TransactionStatusUtil.fromString(json['status']),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      '_id': id,
+      'items': items.map((e) => e.toJson()).toList(),
+      'total': total,
+      'transactionDate': transactionDate.toIso8601String(),
+      'expiredAt': expiredAt.toIso8601String(),
+      'status': TransactionStatusUtil.textOf(status),
+    };
+  }
+}
