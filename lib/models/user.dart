35ad3b7af3b0fd559cb1da021a3708fb904f6809
sammmms <samuelonasis23@gmail.com>
2024-06-27 16:29:37 +0700

    feat: add user copy with method

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -40,4 +40,26 @@ class User {
       "birthday": birthday.toIso8601String(),
     };
   }
+
+  User copyWith({
+    String? objectId,
+    String? name,
+    String? email,
+    String? phone,
+    DateTime? birthday,
+    String? gender,
+    String? id,
+    String? pictureLink,
+  }) {
+    return User(
+      objectId: objectId ?? this.objectId,
+      name: name ?? this.name,
+      email: email ?? this.email,
+      phone: phone ?? this.phone,
+      birthday: birthday ?? this.birthday,
+      gender: gender ?? this.gender,
+      id: id ?? this.id,
+      pictureLink: pictureLink ?? this.pictureLink,
+    );
+  }
 }
