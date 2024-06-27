89dccf5af4e0fb015c9a3ea629d26adc771ee57b
sammmms <samuelonasis23@gmail.com>
2024-06-27 16:59:55 +0700

    chore: removing unused import

--- lib/component/check_component.dart
+++ lib/component/check_component.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class CircleCheckboxComponent extends StatelessWidget {
   final void Function() onClickFunction;
