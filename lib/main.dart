ec392b7ba62fc849c4720c27741d74c1fdd3dba9
sammmms <samuelonasis23@gmail.com>
2024-06-27 16:28:32 +0700

    refactor: moving initializtion to main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,65 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
+import 'package:rxdart/rxdart.dart';
 import 'package:travellingo/bloc/auth/auth_bloc.dart';
 import 'package:travellingo/bloc/auth/auth_state.dart';
 import 'package:travellingo/pages/dashboard_page.dart';
-import 'package:travellingo/utils/locales/locale.dart';
 import 'package:travellingo/pages/login/login_page.dart';
 import 'package:flutter_localization/flutter_localization.dart';
+import 'package:travellingo/splash_page.dart';
+import 'package:travellingo/utils/locales/locale.dart';
+import 'package:travellingo/utils/store.dart';
 import 'package:travellingo/utils/theme_data/light_theme.dart';
 
 final navigatorKey = GlobalKey<NavigatorState>();
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  // dotenv
   await dotenv.load(fileName: ".env");
-  runApp(const MyApp());
+
+  if (kDebugMode) {
+    print('BASE_URL: ${dotenv.env['BASE_URL']}');
+  }
+
+  // LOCALIZATION
+  final languageCode = await Store.getLanguage() ?? 'en';
+
+  // AUTH BLOC
+  final authBloc = AuthBloc();
+
+  await authBloc.checkLogin();
+
+  FlutterNativeSplash.remove();
+
+  Widget app = MultiProvider(
+    providers: [
+      Provider<AuthBloc>.value(
+        value: authBloc,
+      ),
+    ],
+    child: MyApp(
+      languageCode: languageCode,
+    ),
+  );
+
+  runApp(app);
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  final String languageCode;
+  const MyApp({super.key, required this.languageCode});
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  final FlutterLocalization localization = FlutterLocalization.instance;
-  AuthBloc authBloc = AuthBloc();
+  final localization = FlutterLocalization.instance;
 
   @override
   void initState() {
@@ -37,39 +68,36 @@ class _MyAppState extends State<MyApp> {
         const MapLocale('en', AppLocale.en),
         const MapLocale('id', AppLocale.id),
       ],
-      initLanguageCode: 'en',
+      initLanguageCode: widget.languageCode,
     );
-    localization.onTranslatedLanguage = (locale) {
+    localization.onTranslatedLanguage = (locale) async {
+      if (kDebugMode) {
+        print('Language has been changed to $locale');
+      }
+      await Store.saveLanguage(locale!.languageCode);
       setState(() {});
     };
-    SchedulerBinding.instance.addPostFrameCallback(
-      (_) async {
-        await authBloc.checkLogin();
-        FlutterNativeSplash.remove();
-      },
-    );
-
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-        providers: [Provider<AuthBloc>.value(value: authBloc)],
-        child: MaterialApp(
-            navigatorKey: navigatorKey,
-            supportedLocales: localization.supportedLocales,
-            localizationsDelegates: localization.localizationsDelegates,
-            title: 'Travellingo',
-            theme: lightTheme,
-            home: StreamBuilder<AuthState>(
-              stream: authBloc.controller.stream,
-              builder: (context, snapshot) {
-                if (snapshot.data?.receivedToken != null) {
-                  return const DashboardPage();
-                }
-                return const SignInPage();
-              },
-            )));
+    final localization = FlutterLocalization.instance;
+    final authBloc = context.watch<AuthBloc>();
+    return MaterialApp(
+        navigatorKey: navigatorKey,
+        supportedLocales: localization.supportedLocales,
+        localizationsDelegates: localization.localizationsDelegates,
+        title: 'Travellingo',
+        theme: lightTheme,
+        home: StreamBuilder<AuthState>(
+            stream: authBloc.controller.stream,
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return const SplashPage();
+              }
+
+              return const DashboardPage();
+            }));
   }
 }
