cb64185f2e8841350d2d7d542e36f0efdf2abb64
sammmms <samuelonasis23@gmail.com>
2024-07-02 09:49:29 +0700

    feat: added image loader component

---
+++ lib/component/picture_loader_component.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:travellingo/utils/picture_type_util.dart';
+
+class MyImageLoader extends StatelessWidget {
+  final double? width;
+  final double? height;
+  final String url;
+  final PictureType pictureType;
+  final BoxFit fit;
+  const MyImageLoader(
+      {super.key,
+      this.width,
+      this.height,
+      required this.url,
+      this.fit = BoxFit.cover,
+      this.pictureType = PictureType.link});
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeInImage(
+      placeholder: const AssetImage("assets/images/placeholder.png"),
+      image: pictureType == PictureType.link
+          ? NetworkImage(url)
+          : MemoryImage(base64Decode(url)) as ImageProvider,
+      imageErrorBuilder: (context, error, stackTrace) =>
+          Image.asset("assets/image/placeholder.png"),
+      fit: fit,
+      width: width,
+      height: height,
+    );
+  }
+}
