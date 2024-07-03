116a0d4b94e7233c3ba1518311df3b9a2842a3f4
sammmms <samuelonasis23@gmail.com>
2024-07-03 16:46:42 +0700

    chore: renaming airplane class

---
+++ lib/utils/flight_class_util.dart
@@ -0,0 +1,20 @@
+enum FlightClass { economy, business, first, executive, all }
+
+class FlightClassUtil {
+  static const Map<FlightClass, String> _airplaneClass = {
+    FlightClass.economy: "economy",
+    FlightClass.business: "business",
+    FlightClass.first: "first",
+    FlightClass.executive: "executive",
+    FlightClass.all: "all"
+  };
+
+  static FlightClass classFromString(String value) {
+    return FlightClass.values
+        .firstWhere((e) => _airplaneClass[e] == value.toLowerCase());
+  }
+
+  static String stringFromClass(FlightClass value) {
+    return _airplaneClass[value]!;
+  }
+}
