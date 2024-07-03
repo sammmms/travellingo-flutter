a0d7633bd8dbe18d06c3ace2b6597fc643d24932
sammmms <samuelonasis23@gmail.com>
2024-07-03 16:50:46 +0700

    feat: more reactive passenger card

--- lib/pages/flight/select_seat/widget/seat_passenger_card.dart
+++ lib/pages/flight/select_seat/widget/seat_passenger_card.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:travellingo/models/flight.dart';
+import 'package:travellingo/pages/flight/flight_detail/flight_detail_page.dart';
+import 'package:travellingo/utils/flight_class_util.dart';
 
 class SeatPassengerCard extends StatelessWidget {
-  const SeatPassengerCard({super.key});
+  final Flight flight;
+  final Passenger passenger;
+  final String selectedSeat;
+  const SeatPassengerCard(
+      {super.key,
+      required this.selectedSeat,
+      required this.passenger,
+      required this.flight});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 10),
       padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
       decoration: BoxDecoration(
         border: Border.all(
             color: const Color(0xFF28527A), width: 1.0), // Garis outline
-        color: Colors.white,
+        color: Theme.of(context).colorScheme.surfaceBright,
         borderRadius: BorderRadius.circular(4),
         boxShadow: [
           BoxShadow(
@@ -21,50 +32,45 @@ class SeatPassengerCard extends StatelessWidget {
           ),
         ],
       ),
-      child: const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Expanded(
               child: Column(
+                mainAxisSize: MainAxisSize.min,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    '1. Luaman Guaamin',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500, // Medium weight
-                      fontFamily: 'Inter', // Pastikan font Inter tersedia
-                    ),
+                    passenger.fullName,
+                    style: Theme.of(context).textTheme.titleMedium,
                   ),
-                  SizedBox(height: 4.0),
+                  const SizedBox(height: 4.0),
                   Row(
                     children: [
                       Text(
-                        "Economy",
-                        style: TextStyle(
-                            color: Color(0xFF8C8D89),
-                            fontSize: 14,
-                            fontFamily: 'Inter'),
+                        FlightClassUtil.stringFromClass(flight.flightClass),
                       ),
-                      SizedBox(width: 16),
-                      Icon(Icons.circle, color: Color(0xFF8C8D89), size: 8),
-                      SizedBox(width: 16),
-                      Text(
-                        "8D",
-                        style: TextStyle(
-                            color: Color(0xFF8C8D89),
+                      const SizedBox(width: 16),
+                      Icon(Icons.circle,
+                          color: Theme.of(context).colorScheme.onSurface,
+                          size: 8),
+                      const SizedBox(width: 16),
+                      Text("Seat $selectedSeat",
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onSurface,
                             fontSize: 14,
-                            fontFamily: 'Inter'),
-                      ),
+                            fontWeight: FontWeight.w500,
+                          )),
                     ],
                   ),
                 ],
               ),
             ),
-            Icon(Icons.check_circle, color: Color(0xFF57A3BB), size: 20.0),
+            if (passenger.seat.isNotEmpty)
+              Icon(Icons.check_circle,
+                  color: Theme.of(context).colorScheme.tertiary, size: 20.0),
           ],
         ),
       ),
