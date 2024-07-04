8050333b895272a7ed86ec2499bc79ef8ae300e3
sammmms <samuelonasis23@gmail.com>
2024-07-04 16:29:56 +0700

    feat: added more options to image loader

--- lib/component/my_image_loader.dart
+++ lib/component/my_image_loader.dart
@@ -9,6 +9,8 @@ class MyImageLoader extends StatelessWidget {
   final String url;
   final PictureType pictureType;
   final BoxFit fit;
+  final bool darken;
+  final double? roundedRadius;
   const MyImageLoader({
     super.key,
     this.width,
@@ -16,23 +18,30 @@ class MyImageLoader extends StatelessWidget {
     required this.url,
     this.fit = BoxFit.cover,
     this.pictureType = PictureType.link,
+    this.darken = false,
+    this.roundedRadius,
   });
 
   @override
   Widget build(BuildContext context) {
-    return FadeInImage(
-      placeholder: const AssetImage("assets/images/placeholder.png"),
-      image: pictureType == PictureType.link
-          ? NetworkImage(url)
-          : MemoryImage(base64Decode(url)) as ImageProvider,
-      imageErrorBuilder: (context, error, stackTrace) => Image.asset(
-          "assets/images/placeholder.png",
-          fit: fit,
-          width: width,
-          height: height),
-      fit: fit,
-      width: width,
-      height: height,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(roundedRadius ?? 0),
+      child: FadeInImage(
+        placeholder: const AssetImage("assets/images/placeholder.png"),
+        image: pictureType == PictureType.link
+            ? NetworkImage(url)
+            : MemoryImage(base64Decode(url)) as ImageProvider,
+        imageErrorBuilder: (context, error, stackTrace) => Image.asset(
+            "assets/images/placeholder.png",
+            fit: fit,
+            width: width,
+            height: height),
+        fit: fit,
+        width: width,
+        height: height,
+        color: darken ? Colors.black.withAlpha(70) : null,
+        colorBlendMode: darken ? BlendMode.darken : null,
+      ),
     );
   }
 }
