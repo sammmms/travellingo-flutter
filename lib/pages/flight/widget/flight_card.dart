113ab6f68bb970454c59b0b32526ea62201187d2
sammmms <samuelonasis23@gmail.com>
2024-07-05 14:04:09 +0700

    feat: use recent flight search in flight card

--- lib/pages/flight/widget/flight_card.dart
+++ lib/pages/flight/widget/flight_card.dart
@@ -1,52 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localization/flutter_localization.dart';
+import 'package:intl/intl.dart';
+import 'package:travellingo/models/recent_flight_search.dart';
+import 'package:travellingo/utils/flight_class_util.dart';
 
-class FlightCard extends StatelessWidget {
-  // final Flight flight;
-  const FlightCard({super.key});
+class RecentFlightCard extends StatelessWidget {
+  final RecentFlightSearch recentFlight;
+  final Function() onTap;
+  const RecentFlightCard(
+      {super.key, required this.recentFlight, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 300,
-      margin: const EdgeInsets.only(left: 18, right: 8),
-      child: Card(
-        surfaceTintColor: Colors.transparent,
-        borderOnForeground: true,
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(6),
-            side: BorderSide(color: Colors.grey.shade300)),
-        color: Theme.of(context).colorScheme.surfaceBright,
-        child: Padding(
-          padding: const EdgeInsets.all(18),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const Text("Kobe", style: TextStyle(fontSize: 14)),
-                  const SizedBox(width: 8),
-                  Icon(
-                    Icons.arrow_forward,
-                    size: 18,
-                    color: Theme.of(context).colorScheme.onSurface,
-                  ),
-                  const SizedBox(width: 8),
-                  const Text("Arima Onsen", style: TextStyle(fontSize: 14)),
-                  const Spacer(),
-                  Icon(
-                    Icons.arrow_forward_ios,
-                    size: 18,
-                    color: Theme.of(context).colorScheme.onSurface,
-                  ),
-                ],
-              ),
-              const SizedBox(height: 12),
-              const Text("24 Feb 2023 • 1 passenger • Economy",
-                  style: TextStyle(color: Color(0xFF8C8D89), fontSize: 12)),
-            ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 300,
+        margin: const EdgeInsets.only(left: 18, right: 8),
+        child: Card(
+          surfaceTintColor: Colors.transparent,
+          borderOnForeground: true,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(6),
+              side: BorderSide(color: Colors.grey.shade300)),
+          color: Theme.of(context).colorScheme.surfaceBright,
+          child: Padding(
+            padding: const EdgeInsets.all(18),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(recentFlight.from,
+                        style: const TextStyle(fontSize: 14)),
+                    const SizedBox(width: 8),
+                    Icon(
+                      Icons.arrow_forward,
+                      size: 18,
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
+                    const SizedBox(width: 8),
+                    Text(recentFlight.to, style: const TextStyle(fontSize: 14)),
+                    const Spacer(),
+                    Icon(
+                      Icons.arrow_forward_ios,
+                      size: 18,
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 12),
+                Text(
+                    "${DateFormat('dd MMM yyyy').format(recentFlight.date)} • ${recentFlight.passengerCount} ${'passenger'.getString(context)} • ${FlightClassUtil.stringFromClass(recentFlight.flightClass).getString(context)}",
+                    style: const TextStyle(
+                        color: Color(0xFF8C8D89), fontSize: 12)),
+                const SizedBox(height: 12),
+                Text(DateFormat('dd MMMM yyyy HH:mm')
+                    .format(recentFlight.createdAt!))
+              ],
+            ),
           ),
         ),
       ),
