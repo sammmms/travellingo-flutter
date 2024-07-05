3c460311b35c4c39d2759da54d1a0a277703a8d9
sammmms <samuelonasis23@gmail.com>
2024-07-05 11:37:37 +0700

    chore: notification page

--- lib/pages/notification_page.dart
+++ lib/pages/notification_page.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localization/flutter_localization.dart';
 
 class NotificationPage extends StatelessWidget {
   const NotificationPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("notification".getString(context)),
+      ),
+    );
   }
 }
