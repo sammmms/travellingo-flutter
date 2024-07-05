3ae9ae87a726be97e97591c63870fe24f4a244fc
Sam! <156488637+sammmms@users.noreply.github.com>
2024-07-06 01:37:30 +0700

    fix: coloring and localization in checkout protection

--- lib/pages/flight_checkout/widget/flight_checkout_protection.dart
+++ lib/pages/flight_checkout/widget/flight_checkout_protection.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localization/flutter_localization.dart';
 
 class FlightExtraProtection extends StatelessWidget {
   const FlightExtraProtection({super.key});
@@ -10,62 +10,59 @@ class FlightExtraProtection extends StatelessWidget {
       decoration: BoxDecoration(
         border: Border.all(
             color: Colors.grey.withOpacity(0.3), width: 1.0), // Garis outline
-        color: Colors.white,
+        color: Theme.of(context).colorScheme.surfaceBright,
         borderRadius: BorderRadius.circular(16),
       ),
-      child: SizedBox(
-        height: 145, // Fixed height for the card
-        child: Padding(
-          padding: const EdgeInsets.all(12),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment
-                .start, // Aligns children at the start of the row's cross axis
-            children: <Widget>[
-              const Icon(
-                Icons.verified_user, // Replace with your actual protection icon
-                color: Color(0xFFFF7A1A),
-                size: 24,
-              ),
-              const SizedBox(width: 12), // Spacing between icon and title
-              Expanded(
-                child: Column(
-                  mainAxisSize:
-                      MainAxisSize.min, // Align the column content to the top
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    const Text(
-                      'Extra Protection',
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment
+              .start, // Aligns children at the start of the row's cross axis
+          children: <Widget>[
+            const Icon(
+              Icons.verified_user, // Replace with your actual protection icon
+              color: Color(0xFFFF7A1A),
+              size: 24,
+            ),
+            const SizedBox(width: 12), // Spacing between icon and title
+            Expanded(
+              child: Column(
+                mainAxisSize:
+                    MainAxisSize.min, // Align the column content to the top
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'extraProtection'.getString(context),
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Text(
+                    'extraProtectionContent'.getString(context),
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.grey
+                          .withOpacity(0.85), // Set text color for description
+                    ),
+                  ), // Spacing between the description and
+                  const SizedBox(height: 8), // the 'More Info' text
+                  GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                      "moreInfo".getString(context),
                       style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w500,
-                        color: Color(0xFF141511), // Set text color for title
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal,
+                        color: Theme.of(context).colorScheme.tertiary,
                       ),
                     ),
-                    Text(
-                      'Protect your trip to get insurance in the event of an accident. Accident compensation up to \$800.',
-                      style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.normal,
-                        color: Colors.grey.withOpacity(
-                            0.85), // Set text color for description
-                      ),
-                    ), // Spacing between the description and
-                    const SizedBox(height: 8), // the 'More Info' text
-                    RichText(
-                      text: TextSpan(
-                        text: 'More Info',
-                        style: const TextStyle(
-                          color: Color(0xFF3E84A8),
-                          decoration: TextDecoration.none,
-                        ),
-                        recognizer: TapGestureRecognizer()..onTap = () => {},
-                      ),
-                    )
-                  ],
-                ),
+                  )
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
