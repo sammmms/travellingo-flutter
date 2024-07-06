a74e89190f4fee91c79c64fd44fbd32d7f88bb1e
sammmms <samuelonasis23@gmail.com>
2024-07-06 13:13:01 +0700

    feat: added on tap on wishlist card

--- lib/pages/wishlist/widget/wishlist_card.dart
+++ lib/pages/wishlist/widget/wishlist_card.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:travellingo/component/transition_animation.dart';
 import 'package:travellingo/models/place.dart';
+import 'package:travellingo/pages/place_detail/place_detail_page.dart';
 import 'package:travellingo/utils/format_currency.dart';
 import 'package:travellingo/utils/picture_type_util.dart';
 
@@ -10,69 +12,75 @@ class WishlistCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-        margin: const EdgeInsets.symmetric(horizontal: 10),
-        child: SizedBox(
-          height: 125,
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: Row(
-              children: [
-                if (place.pictureType == PictureType.link)
-                  AspectRatio(
-                      aspectRatio: 1,
-                      child: Image.network(place.pictureLink,
-                          width: 100, fit: BoxFit.cover))
-                else if (place.pictureType == PictureType.base64)
-                  AspectRatio(
-                      aspectRatio: 1,
-                      child: Image.memory(base64Decode(place.pictureLink),
-                          width: 100, fit: BoxFit.cover)),
-                const SizedBox(
-                  width: 15,
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          place.name,
-                          style: Theme.of(context).textTheme.titleLarge,
-                        ),
-                        const SizedBox(
-                          height: 5,
-                        ),
-                        Row(
-                          children: [
-                            Icon(Icons.location_on,
-                                size: 15,
-                                color: Theme.of(context).colorScheme.error),
-                            Text("${place.country}, ${place.city}"),
-                          ],
-                        ),
-                        const Spacer(),
-                        Text(
-                          formatToIndonesiaCurrency(place.price),
-                          style: Theme.of(context)
-                              .textTheme
-                              .titleMedium!
-                              .copyWith(
-                                  color:
-                                      Theme.of(context).colorScheme.onSurface,
-                                  fontWeight: FontWeight.bold),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                      ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context, slideInFromBottom(PlaceDetailPage(placeId: place.id)));
+      },
+      child: Card(
+          margin: const EdgeInsets.symmetric(horizontal: 10),
+          child: SizedBox(
+            height: 125,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Row(
+                children: [
+                  if (place.pictureType == PictureType.link)
+                    AspectRatio(
+                        aspectRatio: 1,
+                        child: Image.network(place.pictureLink,
+                            width: 100, fit: BoxFit.cover))
+                  else if (place.pictureType == PictureType.base64)
+                    AspectRatio(
+                        aspectRatio: 1,
+                        child: Image.memory(base64Decode(place.pictureLink),
+                            width: 100, fit: BoxFit.cover)),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            place.name,
+                            style: Theme.of(context).textTheme.titleLarge,
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Row(
+                            children: [
+                              Icon(Icons.location_on,
+                                  size: 15,
+                                  color: Theme.of(context).colorScheme.error),
+                              Text("${place.country}, ${place.city}"),
+                            ],
+                          ),
+                          const Spacer(),
+                          Text(
+                            formatToIndonesiaCurrency(place.price),
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleMedium!
+                                .copyWith(
+                                    color:
+                                        Theme.of(context).colorScheme.onSurface,
+                                    fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ));
+          )),
+    );
   }
 }
