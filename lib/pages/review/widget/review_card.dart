26bf8a37aeb9abb1ec709c20dd70ba49b48dd6f9
sammmms <samuelonasis23@gmail.com>
2024-07-06 12:40:05 +0700

    feat: added review card

---
+++ lib/pages/review/widget/review_card.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localization/flutter_localization.dart';
+import 'package:intl/intl.dart';
+import 'package:travellingo/component/transition_animation.dart';
+import 'package:travellingo/models/review.dart';
+import 'package:travellingo/pages/place_detail/place_detail_page.dart';
+import 'package:travellingo/pages/place_detail/widget/place_review_star.dart';
+
+class ReviewCard extends StatelessWidget {
+  final Review review;
+  const ReviewCard({super.key, required this.review});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            slideInFromRight(
+              PlaceDetailPage(placeId: review.place),
+            ));
+      },
+      child: Card(
+        margin: EdgeInsets.zero,
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  PlaceReviewStar(reviewCount: review.rating),
+                  Text(DateFormat('d/m/yyyy').format(review.createdAt)),
+                ],
+              ),
+              if (review.haveUpdated) ...[
+                const SizedBox(height: 5),
+                Text("edited".getString(context))
+              ],
+              const SizedBox(
+                height: 10,
+              ),
+              const Divider(),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(review.review, maxLines: 2, overflow: TextOverflow.ellipsis)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
