a98a43f94d1364c6dfecf33c1ee4f9dcf7fad36c
sammmms <samuelonasis23@gmail.com>
2024-07-06 12:34:43 +0700

    feat: book flight method

--- lib/bloc/flight/flight_bloc.dart
+++ lib/bloc/flight/flight_bloc.dart
@@ -4,8 +4,10 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:travellingo/bloc/auth/auth_bloc.dart';
 import 'package:travellingo/bloc/flight/flight_state.dart';
+import 'package:travellingo/bloc/mopay/mopay_bloc.dart';
 import 'package:travellingo/interceptors/token_interceptor.dart';
 import 'package:travellingo/models/flight.dart';
+import 'package:travellingo/models/passenger.dart';
 import 'package:travellingo/utils/app_error.dart';
 import 'package:travellingo/utils/error_print.dart';
 import 'package:travellingo/utils/flight_class_util.dart';
@@ -85,6 +87,51 @@ class FlightBloc {
     } catch (err) {
       printError(err: err, method: "getFlight");
       _updateError(err);
+    }
+  }
+
+  Future<dynamic> bookFlight({
+    required Flight flight,
+    required List<Passenger> passengers,
+    required String phoneNumber,
+    int? additionalPayment,
+  }) async {
+    try {
+      _updateStream(FlightState.loading());
+
+      var mopayUserId =
+          await MopayBloc().getMopayIdByPhoneNumber("0$phoneNumber");
+
+      if (mopayUserId is AppError) {
+        return _updateError(mopayUserId);
+      }
+
+      Map<String, dynamic> data = {
+        'passengers': passengers.map((e) => e.toJson()).toList(),
+        'mopayUserId': mopayUserId,
+        if (additionalPayment != null) 'additionalPayment': additionalPayment,
+      };
+
+      if (kDebugMode) {
+        print(data);
+      }
+
+      String url = '/flight/${flight.id}/book';
+
+      if (kDebugMode) {
+        print(url);
+      }
+
+      var response = await dio.post(url, data: data);
+
+      if (kDebugMode) {
+        print(response);
+      }
+
+      return response.data;
+    } catch (err) {
+      printError(err: err, method: "bookFlight");
+      return _updateError(err);
     }
   }
 }
